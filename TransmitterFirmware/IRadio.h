8a448e7ba556218246b775478fc951a1c366c22a
Dan Nixon <dan@dan-nixon.com>
2015-04-11 13:35:19 +0100

    Do not use config for radio

--- TransmitterFirmware/IRadio.h
+++ TransmitterFirmware/IRadio.h
@@ -20,11 +20,6 @@ public:
   virtual bool setChannel(channelid_t id, channelvalue_t value) = 0;
   virtual bool tx() = 0;
 
-  virtual size_t getOptionCount() = 0;
-  virtual char * getOptionName(int id) = 0;
-  virtual int16_t * getOptionValue(int id) = 0;
-  virtual bool setOption(int id, int16_t value) = 0;
-
 };
 
 #endif
