ae28269ea58ef0df25a8082fb3123a18791e0dea
Dan Nixon <dan@dan-nixon.com>
2016-01-30 14:33:49 +0000

    docs

--- AliceFirmware/cppm_encode.cpp
+++ AliceFirmware/cppm_encode.cpp
@@ -1,24 +1,42 @@
+/** @file */
+
 #include "cppm_encode.h"
 
 #include <TimerThree.h>
+
+/**
+ * @def TIMER
+ * @brief The timer in use for triggering the CPPM ISR.
+ */
 #define TIMER Timer3
 
+/**
+ * @brief True if the timer has been started for CPPM ISR.
+ */
 bool cppm_running = false;
 
+/**
+ * @brief Holds state of each CPPM stream.
+ */
 volatile struct
 {
-  bool enable;
-  int8_t pin;
-  uint16_t values[MAX_PPM_CHANNELS];
-  size_t num_channels;
-  uint64_t frame_length;
-  uint64_t pulse_length;
-  uint8_t logic;
-  uint8_t channel;
-  uint64_t frame_start_time;
-  uint64_t channel_start_time;
+  bool enable; //!< Flag to indicate this stream is active and should be output
+  int8_t pin; //!< Pin this stream outputs on
+  usvalue_t values[MAX_PPM_CHANNELS]; //!< Values (in us) for channels
+  size_t num_channels; //!< Number of active channels on this stream
+  uint64_t frame_length; //!< Length of a frame in us
+  uint64_t pulse_length; //!< Length of a channel marker pulse in us
+  uint8_t logic; //!< Active logic level
+
+  uint8_t channel; //!< Current channel being output in ISR
+  uint64_t frame_start_time; //!< Start of frame in us in ISR
+  uint64_t channel_start_time; //!< Start of channel pulse in us in ISR
 } cppm_streams[NUM_PPM_STREAMS];
 
+/**
+ * @brief Initializes the CPPM driver and assigns reasonable initial values to
+ *        the configuration.
+ */
 void cppm_init()
 {
   cppm_stop();
@@ -38,6 +56,13 @@ void cppm_init()
   }
 }
 
+/**
+ * @brief Starts the CPPM driver.
+ * @return True if the CPPM driver was started successfully, false otherwise
+ * @see cppm_stop()
+ *
+ * No further configuration changes may be made until cppm_stop() is called.
+ */
 bool cppm_start()
 {
   if (cppm_running)
@@ -62,6 +87,11 @@ bool cppm_start()
   return true;
 }
 
+/**
+ * @brief Stops the CPPM driver.
+ * @return True if the CPPM driver was stopped successfully, false otherwise
+ * @see cppm_start()
+ */
 bool cppm_stop()
 {
   if (!cppm_running)
@@ -73,6 +103,9 @@ bool cppm_stop()
   return true;
 }
 
+/**
+ * @brief CPPM ISR, called by the TIMER.
+ */
 void cppm_isr()
 {
   uint32_t t = micros();
@@ -115,6 +148,15 @@ void cppm_isr()
   }
 }
 
+/**
+ * @brief Enables or disables output of a given CPPM stream.
+ * @param streamID Stream number
+ * @param enable If the stream should be output
+ * @return True if configuration was set, false otherwise
+ * @see cppm_stop()
+ *
+ * CPPM output must be stopped before configuration changes can be made.
+ */
 bool cppm_set_stream_enable(size_t streamID, bool enable)
 {
   if (cppm_running)
@@ -127,6 +169,15 @@ bool cppm_set_stream_enable(size_t streamID, bool enable)
   return true;
 }
 
+/**
+ * @brief Sets the pin that the CPPM stream will be output on.
+ * @param streamID Stream number
+ * @param pin Arduino pin number
+ * @return True if configuration was set, false otherwise
+ * @see cppm_stop()
+ *
+ * CPPM output must be stopped before configuration changes can be made.
+ */
 bool cppm_set_stream_pin(size_t streamID, uint8_t pin)
 {
   if (cppm_running)
@@ -139,6 +190,15 @@ bool cppm_set_stream_pin(size_t streamID, uint8_t pin)
   return true;
 }
 
+/**
+ * @brief Sets the number of active channels on a given CPPM stream.
+ * @param streamID Stream number
+ * @param numChannels Number of active channels
+ * @return True if configuration was set, false otherwise
+ * @see cppm_stop()
+ *
+ * CPPM output must be stopped before configuration changes can be made.
+ */
 bool cppm_set_stream_num_channels(size_t streamID, size_t numChannels)
 {
   if (cppm_running)
@@ -151,6 +211,17 @@ bool cppm_set_stream_num_channels(size_t streamID, size_t numChannels)
   return true;
 }
 
+/**
+ * @brief Sets the logic direction for CPPM pulses.
+ * @param streamID Stream number
+ * @param logic Active logic level.
+ * @return True if configuration was set, false otherwise
+ * @see cppm_stop()
+ *
+ * Set logic level will be the logic level used for the channel marker pulses.
+ *
+ * CPPM output must be stopped before configuration changes can be made.
+ */
 bool cppm_set_stream_logic(size_t streamID, uint8_t logic)
 {
   if (cppm_running)
@@ -163,6 +234,15 @@ bool cppm_set_stream_logic(size_t streamID, uint8_t logic)
   return true;
 }
 
+/**
+ * @brief Sets the length of a CPPM frame in microseconds.
+ * @param streamID Stream number
+ * @param frameLength Frame length in us
+ * @return True if configuration was set, false otherwise
+ * @see cppm_stop()
+ *
+ * CPPM output must be stopped before configuration changes can be made.
+ */
 bool cppm_set_stream_frame_length(size_t streamID, uint64_t frameLength)
 {
   if (cppm_running)
@@ -175,6 +255,15 @@ bool cppm_set_stream_frame_length(size_t streamID, uint64_t frameLength)
   return true;
 }
 
+/**
+ * @brief Sets the length of a channel marker pulse in microseconds.
+ * @param streamID Stream number
+ * @param pulseLength Pulse length in us
+ * @return True if configuration was set, false otherwise
+ * @see cppm_stop()
+ *
+ * CPPM output must be stopped before configuration changes can be made.
+ */
 bool cppm_set_stream_pulse_length(size_t streamID, uint64_t pulseLength)
 {
   if (cppm_running)
@@ -187,7 +276,13 @@ bool cppm_set_stream_pulse_length(size_t streamID, uint64_t pulseLength)
   return true;
 }
 
-void cppm_set_stream_channel(size_t streamID, uint8_t channel, uint16_t value)
+/**
+ * @brief Sets the value of a channel of a given stream.
+ * @param streamID Stream number
+ * @param channel Channel number
+ * @param value New value for the channel (between 1000 and 2000)
+ */
+void cppm_set_stream_channel(size_t streamID, uint8_t channel, usvalue_t value)
 {
   noInterrupts();
   cppm_streams[streamID].values[channel] = value;
--- AliceFirmware/cppm_encode.h
+++ AliceFirmware/cppm_encode.h
@@ -1,9 +1,20 @@
+/** @file */
+
 #ifndef _ALICE_CPPM_ENCODE_H_
 #define _ALICE_CPPM_ENCODE_H_
 
 #include "AliceTypes.h"
 
+/**
+ * @def MAX_PPM_CHANNELS
+ * @brief Maximum number of channels that can be output by a single stream.
+ */
 #define MAX_PPM_CHANNELS 8
+
+/**
+ * @def NUM_PPM_STREAMS
+ * @brief Maximum number of PPM streams.
+ */
 #define NUM_PPM_STREAMS 4
 
 void cppm_init();
@@ -17,6 +28,6 @@ bool cppm_set_stream_num_channels(size_t streamID, size_t numChannels);
 bool cppm_set_stream_logic(size_t streamID, uint8_t logic);
 bool cppm_set_stream_frame_length(size_t streamID, uint64_t frameLength);
 bool cppm_set_stream_pulse_length(size_t streamID, uint64_t pulseLength);
-void cppm_set_stream_channel(size_t streamID, uint8_t channel, uint16_t value);
+void cppm_set_stream_channel(size_t streamID, uint8_t channel, usvalue_t value);
 
 #endif
